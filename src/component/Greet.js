570a1770238dad77a97a4dc496ba983fde84bbd2
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-05-10 21:06:18 +0530

    first commit

---
+++ src/component/Greet.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Greet = () => {
+  return (
+    <h2>Hello Greet Component</h2>
+  )
+}
+
+export default Greet;
---
+++ src/component/Welcome.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+class Welcome extends Component{
+  render(){
+    return <h3>In Welcome class component</h3>
+  }
+}
+
+export default Welcome;
